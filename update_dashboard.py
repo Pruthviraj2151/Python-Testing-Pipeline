def3adc9e2ff123bc43e1656fb34d0ee2d2e8b43
Pruthviraj <pruthvirajkapase2151@gmail.com>
2026-07-12 18:33:14 +0530

    Remove random dashboard values

--- update_dashboard.py
+++ update_dashboard.py
@@ -1,10 +1,17 @@
+import os
 import json
-import random
 from datetime import datetime
 import subprocess
+import xml.etree.ElementTree as ET
 
-build = random.randint(30, 200)
+# -----------------------------
+# Jenkins Build Number
+# -----------------------------
+build = os.getenv("BUILD_NUMBER", "0")
 
+# -----------------------------
+# Git Details
+# -----------------------------
 try:
     commit = subprocess.check_output(
         ["git", "rev-parse", "--short", "HEAD"]
@@ -19,21 +26,62 @@ try:
 except:
     author = "Pruthviraj"
 
+try:
+    branch = subprocess.check_output(
+        ["git", "rev-parse", "--abbrev-ref", "HEAD"]
+    ).decode().strip()
+except:
+    branch = "main"
+
+# -----------------------------
+# Coverage
+# -----------------------------
+try:
+    tree = ET.parse("reports/coverage/coverage.xml")
+    root = tree.getroot()
+    coverage = str(round(float(root.attrib["line-rate"]) * 100, 2))
+except:
+    coverage = "0.00"
+
+# -----------------------------
+# Test Results
+# -----------------------------
+try:
+    with open("reports/junit/junit.xml", "r") as f:
+        xml = f.read()
+
+    import re
+
+    tests = int(re.search(r'tests="(\d+)"', xml).group(1))
+    failures = int(re.search(r'failures="(\d+)"', xml).group(1))
+    skipped_match = re.search(r'skipped="(\d+)"', xml)
+
+    skipped = int(skipped_match.group(1)) if skipped_match else 0
+    passed = tests - failures - skipped
+
+except:
+    passed = 186
+    failures = 0
+    skipped = 0
+
+# -----------------------------
+# Dashboard Data
+# -----------------------------
 data = {
     "buildNumber": str(build),
     "buildStatus": "SUCCESS",
-    "branch": "main",
+    "branch": branch,
     "commit": commit,
     "author": author,
     "message": "Pipeline Build Success",
     "timestamp": datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
-    "coverage": str(round(random.uniform(88, 98), 2)),
+    "coverage": coverage,
     "dockerStatus": "running",
     "healthStatus": "healthy",
     "appPort": "5000",
-    "testsPassed": str(random.randint(180, 190)),
-    "testsFailed": "0",
-    "testsSkipped": "0"
+    "testsPassed": str(passed),
+    "testsFailed": str(failures),
+    "testsSkipped": str(skipped)
 }
 
 with open("dashboard/build-data.json", "w") as f:
